0d545775568fcd3902e56f3936dfb79146ad24e8
Hyune-c <choi8608@gmail.com>
2023-01-11 04:05:52 +0900

    ✅ PerformanceTest

---
+++ src/test/kotlin/com/example/perf16digitcodegenerator/PerformanceTest.kt
@@ -0,0 +1,48 @@
+package com.example.perf16digitcodegenerator
+
+import com.example.perf16digitcodegenerator.refactor.CodeGenerator
+import com.example.perf16digitcodegenerator.refactor.strategy.LegacyWithJavaRandomStrategy
+import com.example.perf16digitcodegenerator.refactor.strategy.LegacyWithKotlinRandomStrategy
+import com.example.perf16digitcodegenerator.refactor.strategy.LegacyWithThreadLocalRandomStrategy
+import com.example.perf16digitcodegenerator.refactor.strategy.UUIDStrategy
+import com.example.perf16digitcodegenerator.refactor.strategy.UUIDWithSequenceStrategy
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.string.shouldBeUpperCase
+import io.kotest.matchers.string.shouldNotContain
+import kotlin.system.measureTimeMillis
+
+class PerformanceTest : FunSpec({
+    val testSizes = listOf(10_000L, 1_000_000L, 5_000_000L, 10_000_000L)
+    val legacyWithThreadLocalRandomGenerator = CodeGenerator(LegacyWithThreadLocalRandomStrategy())
+    val legacyWithJavaRandomGenerator = CodeGenerator(LegacyWithJavaRandomStrategy())
+    val legacyWithKotlinRandomGenerator = CodeGenerator(LegacyWithKotlinRandomStrategy())
+    val uuidGenerator = CodeGenerator(UUIDStrategy())
+    val uuidWithSequence = CodeGenerator(UUIDWithSequenceStrategy())
+
+    test("performanceTest") {
+        testSizes.forEach {
+            println("### $it loop")
+            println("### legacyWithThreadLocalRandom  = ${measureTimeMillis(legacyWithThreadLocalRandomGenerator, it)} ms")
+            println("### legacyWithJavaRandom         = ${measureTimeMillis(legacyWithJavaRandomGenerator, it)} ms")
+            println("### legacyWithKotlinRandom       = ${measureTimeMillis(legacyWithKotlinRandomGenerator, it)} ms")
+            println("### uuid                         = ${measureTimeMillis(uuidGenerator, it)} ms")
+            println("### uuidWithSequence             = ${measureTimeMillis(uuidWithSequence, it)} ms")
+            println()
+        }
+    }
+})
+
+fun measureTimeMillis(generator: CodeGenerator, size: Long): Long {
+    var codes: Set<String>
+    val elapsed = measureTimeMillis {
+        codes = generator.generate(size)
+    }
+
+    codes.size shouldBe size
+    codes.first() shouldNotContain "-"
+    codes.first().shouldBeUpperCase()
+
+//    println(codes)
+    return elapsed
+}
